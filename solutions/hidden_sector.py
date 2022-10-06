b210d258f309af33cff6e71e91b183104d5c0a47
Diego Restrepo <restrepo@udea.edu.co>
2022-10-06 02:35:12 +0000

    lc

--- solutions/hidden_sector.py
+++ solutions/hidden_sector.py
@@ -1,6 +1,20 @@
 import pandas as pd
-df=pd.read_json('https://zenodo.org/record/5526707/files/solutions.json?download=1')
-print(df.shape)
+import os
+if not os.path.exists('solutions.json'):
+    print('remote loading')
+    df=pd.read_json('https://zenodo.org/record/5526707/files/solutions.json?download=1')
+    df.to_json('solutions.json',orient='records')
+else:
+    print('local loading')
+    df=pd.read_json('solutions.json')
 
+print(df.shape)
+print(df.columns)
 def get_hidden_sector(l):
   return []
+
+
+df['hidden']=df['solution'].apply(get_hidden_sector)
+
+print('*'*20)
+print(df.iloc[0].to_dict())
