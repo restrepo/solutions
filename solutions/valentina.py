0ab7879eeef6b601100f8322779903b19f2a9c6b
Diego Restrepo <restrepo@udea.edu.co>
2022-10-13 15:31:47 +0000

    recover tests

--- solutions/valentina.py
+++ solutions/valentina.py
@@ -53,25 +53,10 @@ def get_hidden_sector(l):
 
     return final
 
-assert get_hidden_sector([1,2,-3,4,5])[0].get('S')==6
-assert get_hidden_sector([1,2,-3,-3,4,5])[0].get('S')==6
-assert get_hidden_sector([1,1,2,-3,-3,4,5,5])[0].get('S')==6
-#aψ1ψ1+bψ1ψ2+cψ1,ψ3 → https://www.wolframalpha.com/input?i=Rank%20{{a,b,c},{b,0,0},{c,0,0}}
-assert get_hidden_sector([1,2,-3,4,5,9,9])==[]
-get_hidden_sector([1,1,2,-3,4,5])
-#aψ1ψ2+bψ1ψ3 → https://www.wolframalpha.com/input?i=rank+{{0,a,b},{a,0,0},{b,0,0}}
-assert get_hidden_sector([1,1,2,-3,4,5])==[]
-assert get_hidden_sector([1,1,2,-3,-3,4,5])==[]
-assert get_hidden_sector([1,2,-3,4,5,8])==[]
-assert get_hidden_sector([1,2,-3,4,5,8,8])==[]
-assert get_hidden_sector([])==[]
-assert get_hidden_sector([1, 1, 1, 1, 1, -2, -2, -2, -2, 3])==[]
-assert get_hidden_sector([1, 2, 2, 2, -3, -5, -6, 7])[0].get('S')==4
-assert get_hidden_sector( [1, 2, 2, 4, -5, -5, -7, 8] )[0].get('S')==3
-# Ana test
-assert get_hidden_sector([2, -3, -4, 5, -6, 7, 7, -8])[0].get('S')==1
-assert get_hidden_sector([3, 5, -8, 9, -10, -14, 15])==[] # Dirac triplet [-10,5,15] (s=5)
-assert get_hidden_sector([3, 5, -8, 9, -10, -14, 15, 20,-30, 35])==[]
+assert get_hidden_sector([2, -3, 4, -5, 11])[0].get('S') == 6
+assert get_hidden_sector([2, 2, -3, 4, 4, -5, 11])[0].get('S') == 6
+assert get_hidden_sector([2, 2, -3, 4, -5, 11]) == []
+assert get_hidden_sector([2, -3, -4, 5, -6, 7, 7, -8])[0].get("S") == 1
   
 # df['hidden']=df['solution'].apply(get_hidden_sector)
 
